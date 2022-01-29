80664dfedd7e5907596933e4e11bcc8dfce93e65
MainaPaula <88315727+MainaPaula@users.noreply.github.com>
2022-01-29 14:58:35 +0300

    First Task

---
+++ max.py
@@ -0,0 +1,7 @@
+def getMax(num1, num2):
+    if(num1 > num2):
+        print("The larger number is {}".format(num1))
+    else:
+        print("The larger number is {}".format(num2))
+
+getMax(45, 98)
